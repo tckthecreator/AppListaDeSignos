90244815d9589a0f057ec78133aa09109291472a
tckthecreator <dsoouuzavitu@gmail.com>
2021-03-19 01:37:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AppListaDeSignos
+ 
---
+++ componentes/Titulo/estilo.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const estilo = StyleSheet.create({
+  box: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 60
+  },
+  texto:{
+    fontSize: 28,
+    padding: 10,
+    color: '#61dafb'
+  }
+});
+
+export default estilo;
